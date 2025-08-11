3cb270a4c50ea131317d12613a5c58ca53c025ce
honesty2223 <honesty220203@gmail.com>
2025-08-11 18:36:14 +0900

    :tada: init

---
+++ src/main/java/com/followbuffett/FollowbuffettApplication.java
@@ -0,0 +1,13 @@
+package com.followbuffett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowbuffettApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowbuffettApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/followbuffett/FollowbuffettApplicationTests.java
@@ -0,0 +1,13 @@
+package com.followbuffett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowbuffettApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
